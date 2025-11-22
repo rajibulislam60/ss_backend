fcd0facd26d518bd55339da7882acf92d5fff0e6
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-11-22 22:06:06 +0600

    multer add done

--- router/api/products.js
+++ router/api/products.js
@@ -1,11 +1,40 @@
 const express = require("express");
+const multer = require("multer");
 const {
   addProductController,
   allProductsController,
 } = require("../../controllers/productsController");
 const router = express.Router();
 
-router.post("/addproduct", addProductController);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const extention = file.originalname.split(".");
+    cb(
+      null,
+      file.fieldname + "-" + uniqueName + `.${extention[extention.length - 1]}`
+    );
+  },
+});
+
+function errCheck(err, req, res, next) {
+  if (err) {
+    return res.status(500).send({ success: false, msg: err.message });
+  }
+  next();
+}
+
+const upload = multer({ storage: storage });
+
+router.post(
+  "/addproduct",
+  upload.array("image"),
+  errCheck,
+  addProductController
+);
 router.get("/allproducts", allProductsController);
 
 module.exports = router;
